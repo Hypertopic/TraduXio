f8830e917c2f47605c76fea2e848cca52e519868
Diana Zambon <diana@dianck-ubuntu.(none)>
2010-05-10 10:54:49 +0200

    Corrected "wrong" number of translations for each original text in the homepage. Only visible translations number is now shown. Image changeed in order to correct visualization of large onglets (in translation read page).

--- application/models/Work.php
+++ application/models/Work.php
@@ -151,8 +151,10 @@ class Model_Work extends Model_Taggable
         $selectOrig = $this->getSelectCondOriginalWork('sentence');
         $selectAlwd = $this->getSelectCondAllowedWork('read'); 
         
+        $selectIntAlwd = $db->select()->from('interpretation',array('original_work_id','work_id'))->where('work_id IN (?)', $selectAlwd);
+        
         $select->distinct()->from(array('work'=>'work'),array('id','title','author','language','created','modified','creator'))
-                        ->joinLeft(array('i'=>'interpretation'),'i.original_work_id = work.id','count(distinct i.work_id)')
+                        ->join(array('i'=>$selectIntAlwd),'i.original_work_id = work.id','count(distinct i.work_id)')
                         ->group(array('work.id','work.title','work.author','work.language','work.created','work.modified','work.creator'))
                         ->where('work.id IN (?)',$selectOrig)->where('work.id IN (?)',$selectAlwd);
         
