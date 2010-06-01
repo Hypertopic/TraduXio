8e015aca5c9a296c9ba4901ba21f423c33e2c0fa
Diana Zambon <diana@dianck-ubuntu.(none)>
2010-06-01 14:21:27 +0200

    Last news correction: Deleted from last news section all tag deletion informations (and addition if deletion is present).

--- application/models/History.php
+++ application/models/History.php
@@ -65,13 +65,12 @@ class Model_History extends Model_Abstract
         $histTable->insert(array('user'=>$user_name,'work_id'=>$id),$code,$params);
     }
     
-    public function getAllRecentHistory($days,$drop=false){
+    public function getAllRecentHistory($days,$drop=true){
         $db = $this->_getTable()->getAdapter();
         $workModel = new Model_Work();
         $selectAlwd = $workModel->getSelectCondAllowedWork('read');         
         $sqlcond = "history.date > current_date - integer '".$days."'";
         $select = $db->select()->from('history',array('user','date','work_id','message'))->join('work','work.id=history.work_id')->where('history.work_id IN (?)',$selectAlwd)->where($sqlcond)->order('date DESC');
-        Tdxio_Log::info($select->__toString(),'perfranck');
         $rows = $db->fetchAll($select);
         $history = $this->extractInfo($rows,'all');
         Tdxio_Log::info($history,'all recent history'); 
@@ -87,9 +86,6 @@ class Model_History extends Model_Abstract
         $genres = $genreModel->getGenres();
         foreach($rows as $key=>$row){
             $msg=unserialize($row['message']);
-            Tdxio_Log::info($row,'row');
-            Tdxio_Log::info($row['message'],'unserialized msg');
-            Tdxio_Log::info($msg,'unserialized msg');
             $history[$key]['message'] = $this->_codeList[$type][$msg['code']];
             if(isset($msg['params'])){
                 $genre_name = __($genres[$msg['params']['genre']]);
@@ -97,41 +93,27 @@ class Model_History extends Model_Abstract
             }
         }
         return $history;        
-    }
-    
+    }    
     
     public function dropRedundancies(array $history){
         krsort($history);//analyze history from the oldest new to the last new
-        Tdxio_Log::info($history,'all recent history reordered');
+        $partHistory = $history;
         foreach($history as $key=>$row){
             if($row['message']==4)
-                unset($history[$key]);//se il tag è di eliminazione di un tag, eliminarlo}
-            elseif($row['message']==3){// se il tag è di aggiunta di un tag, eliminarlo se esiste anche il suo opposto 
-                $droppedRow = $row;
-                $droppedRow['message']=4;
-                unset($droppedRow['date']);
-                if(in_array($droppedRow,$history))
-                    unset($history[$key]);
-            }            
+                unset($history[$key]);//if the new is a tag elimination new, delete it
+            elseif($row['message']==3){//if the new is a tag addition new and the same tag deletion exists, delete the new
+                $stop=false;
+                foreach($partHistory as $index=>$oppRow)
+                {   if($oppRow['message']==4 and $oppRow['work_id']==$row['work_id'] and $oppRow['params']==$row['params'])
+                    {   unset($history[$key]);
+                        $stop=true;
+                    }
+                    if($stop) break;
+                }
+            }
+            unset($partHistory[$key]);
         }
         ksort($history);//reorder the history in the good way
         return $history;
-    }
-    
-    public function equivalent(array $a, array $b, array $paramList){
-        foreach($paramList as $param){
-            if($param=='date'){
-                $adate = getdate(strtotime($a['date']));
-                $bdate = getdate(strtotime($b['date']));
-                Tdxio_Log::info($adate,'date1');
-                Tdxio_Log::info($bdate,'date2');
-                $diff = array_diff(array($adate['year'],$adate['yday']),array($bdate['year'],$bdate['yday']));
-                if(!empty($diff))
-                    return false;
-            }elseif($a[$param]!=$b[$param]){
-                return false;
-            }
-        }
-        return true;
     }
 }
