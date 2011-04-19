d34235a565b745b7cb03c488bcd1f2b4eeeddbaa
Diana Zambon <diana@ubuntu-precision.dianck>
2011-04-19 10:09:49 +0200

    Small correction to the login form

--- application/forms/Login.php
+++ application/forms/Login.php
@@ -3,13 +3,33 @@
 class Form_Login extends Zend_Form
 {
     public function init()
-    {
+    { 
+		
+		/*$name->setOptions(array(
+            'decorators' => array(
+                'ViewHelper',
+                'Label',
+                'Errors',
+                array(array('data'=>'HtmlTag'), array('tag'=>'span')),
+                array('Label', array('tag' => 'span','requiredSuffix'=>' : ','requiredPrefix'=>'*')),
+                array(array('row'=>'HtmlTag'),array('tag'=>'div','class'=>'input-line'))
+            ),
+            'label'      => __('Name'),
+            'required'   => true
+        ));*/
         $username = $this->addElement('text', 'username', array(
             'filters'    => array('StringTrim', 'StringToLower'),
-            'validators' => array(
-            ),
+            'validators' => array(),
             'required'   => true,
-            'label'      => __('Your username').':',
+            'label'      => __('Your username'),
+			'decorators' => array(
+                'ViewHelper',
+                'Label',
+                'Errors',
+                array(array('data'=>'HtmlTag'), array('tag'=>'span')),
+                array('Label', array('tag' => 'span','requiredSuffix'=>' : ','requiredPrefix'=>'*')),
+                array(array('row'=>'HtmlTag'),array('tag'=>'div'))
+            )
         ));
 
         $password = $this->addElement('password', 'password', array(
@@ -17,20 +37,31 @@ class Form_Login extends Zend_Form
             'validators' => array(
             ),
             'required'   => true,
-            'label'      => __('Password').':',
+            'label'      => __('Password'),
+            'decorators' => array(
+                'ViewHelper',
+              //  'Label',
+                'Errors',
+                array(array('data'=>'HtmlTag'), array('tag'=>'span')),
+                array('Label', array('tag' => 'span','requiredSuffix'=>' : ','requiredPrefix'=>'*')),
+                array(array('row'=>'HtmlTag'),array('tag'=>'div'))
+            )
         ));
 
         $password = $this->addElement('hidden', 'redirect', array(
             'validators' => array(
-            )
-            //'required'   => true,
+            ),
+            'decorators' => array(
+                'ViewHelper')
         ));
 
          $login = $this->addElement('submit', 'login', array(
             'required' => false,
             'ignore'   => true,
             'label'    => __('Login'),
-        ));
+            'decorators' => array(
+                'ViewHelper'
+			)));
 
         // We want to display a 'failed authentication' message if necessary;
         // we'll do that with the form 'description', so we need to add that
