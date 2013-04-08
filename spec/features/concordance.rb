736812b6e637e55c8901a5c1f14bdf7ef802567a
Jeremie JALOUZET <jeremie.jalouzet@utt.fr>
2013-04-08 11:29:35 +0200

    TEST: Real example in test

--- spec/features/concordance.rb
+++ spec/features/concordance.rb
@@ -8,27 +8,27 @@ feature 'Do a concordance' do
     visit '/work/index'
     click_on 'Déposer un nouveau texte' 
     fill_in 'Titre', :with => $a_title
-    fill_in 'Auteur', :with => 'TestAuteur'
-    fill_in 'Contenu du Texte', :with => 'Bla bla bla bla test'
+    fill_in 'Auteur', :with => 'Wikipedia (Francais)'
+    fill_in 'Contenu du Texte', :with => "L'IA souligne la difficulté à expliciter toutes les connaissances utiles à la résolution d'un problème complexe."
     select 'Langue Original', :from => 'Français'    
     click_on 'Déposer'
 	click_on 'Créer une traduction'
-	fill_in '*Translator', :with => 'TestTraducteur'
+	fill_in '*Translator', :with => 'Wikipedia (Anglais)'
 	fill_in '*Titre', :with => $a_title + ' (traduction)'
 	select '*Translate to', :from => 'Anglais'
 	click_on 'Déposer'
-	fill_in 'Bloc 1', :with => 'Bla bla bla bla apple'
+	fill_in 'Bloc 1', :with => "AI highlights the difficulty of explaining all the knowledge needed to solve a complex problem."
 	visit '/login/logout'
   end
 
   scenario 'View an extract of a private traduction in a concordance' do
   visit '/translation/concord'
-    fill_in 'Recherche', :with => 'bla bla apple'
+    fill_in 'Recherche', :with => 'AI'
     click_on 'Recherche'
     page.should have_content $a_title + ' (traduction)'
-    page.should have_content 'Bla bla bla bla apple'
-    page.should have_content 'Bla bla bla bla test'
-    page.should have_content 'TestAuteur'
+    page.should have_content "AI highlights the difficulty of explaining all the knowledge needed to solve a complex problem."
+    page.should have_content "L'IA souligne la difficulté à expliciter toutes les connaissances utiles à la résolution d'un problème complexe."
+    page.should have_content 'Wikipedia (Francais)'
   end
 
 end
