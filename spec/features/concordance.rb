bedfaf2850eeae92f536ff616ed366a5fb107028
Jeremie JALOUZET <jeremie.jalouzet@utt.fr>
2013-04-08 11:31:46 +0200

    TEST: Fix example

--- spec/features/concordance.rb
+++ spec/features/concordance.rb
@@ -8,13 +8,13 @@ feature 'Do a concordance' do
     visit '/work/index'
     click_on 'Déposer un nouveau texte' 
     fill_in 'Titre', :with => $a_title
-    fill_in 'Auteur', :with => 'Wikipedia (Francais)'
+    fill_in 'Auteur', :with => 'Wikipedia (Français)'
     fill_in 'Contenu du Texte', :with => "L'IA souligne la difficulté à expliciter toutes les connaissances utiles à la résolution d'un problème complexe."
     select 'Langue Original', :from => 'Français'    
     click_on 'Déposer'
 	click_on 'Créer une traduction'
 	fill_in '*Translator', :with => 'Wikipedia (Anglais)'
-	fill_in '*Titre', :with => $a_title + ' (traduction)'
+	fill_in '*Titre', :with => $a_title + ' (Translation)'
 	select '*Translate to', :from => 'Anglais'
 	click_on 'Déposer'
 	fill_in 'Bloc 1', :with => "AI highlights the difficulty of explaining all the knowledge needed to solve a complex problem."
@@ -25,10 +25,10 @@ feature 'Do a concordance' do
   visit '/translation/concord'
     fill_in 'Recherche', :with => 'AI'
     click_on 'Recherche'
-    page.should have_content $a_title + ' (traduction)'
+    page.should have_content $a_title + ' (Translation)'
     page.should have_content "AI highlights the difficulty of explaining all the knowledge needed to solve a complex problem."
     page.should have_content "L'IA souligne la difficulté à expliciter toutes les connaissances utiles à la résolution d'un problème complexe."
-    page.should have_content 'Wikipedia (Francais)'
+    page.should have_content 'Wikipedia (Français)'
   end
 
 end
