e202d894d1c96d179b804b749b19da74f9a6497b
Jeremie JALOUZET <jeremie.jalouzet@utt.fr>
2013-05-06 10:33:00 +0200

    Updating spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,15 +3,10 @@ require 'capybara/webkit'
 
 Capybara.run_server = false
 Capybara.default_driver = :webkit
-Capybara.app_host = 
-  'http://127.0.0.1:5984/tire-a-part/_design/tire-a-part/_rewrite'
+Capybara.app_host = 'http://traduxio.test.hypertopic.org/'
 
 def a_string()
   s = ('a'..'z').to_a.shuffle[0,8].join
-end
-
-def in_dialog()
-  f = find('.ui-dialog')
 end
 
 def prefer_language(language)
