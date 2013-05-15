dfad16565f209abe5545e8ab010012fee4dc1da6
Paul Aubineau <aubineau.p@gmail.com>
2013-05-15 16:37:28 +0300

    TEST : Comparaison de traductions, update

--- spec/features/compare_traduction.rb
+++ spec/features/compare_traduction.rb
@@ -9,14 +9,15 @@ require 'spec_helper'
 feature 'Navigate and compare translation' do
 	scenario 'navigate and compare translation' do
 		visit '/'
-		#Choose an original work
 		click_on 'The lamp (Fungi from Yuggoth, 6)'
-		#A translation is visible by default
-		page.should have_bloc 'We found the lamp inside those hollow cliffs'
-		page.should have_bloc 'Nous trouvâmes la lampe à l\'intérieur de ces cavités rocheuses'
-		#Open another translation
+		page.should have_content 'We found the lamp inside those hollow cliffs'
+		page.should have_content 'Nous trouvâmes la lampe à l\'intérieur de ces cavités rocheuses'
+		page.should_not have_content 'Nous trouvâmes la lampe à l’intérieur de ces falaises creuses'
 		click_on 'François Truchaud'
-		page.should have_bloc 'We found the lamp inside those hollow cliffs'
-		page.should have_bloc 'Nous trouvâmes la lampe à l’intérieur de ces falaises creuses'
+		page.should have_content 'We found the lamp inside those hollow cliffs'
+		page.should have_content 'Nous trouvâmes la lampe à l\'intérieur de ces cavités rocheuses'
+		page.should have_content 'Nous trouvâmes la lampe à l’intérieur de ces falaises creuses'
+		click_on 'Aurélien Bénel'
+		page.should_not have_content 'Nous trouvâmes la lampe à l\'intérieur de ces cavités rocheuses'
 	end
 end
