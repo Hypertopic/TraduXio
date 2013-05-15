929ede2e0b23e36007ba277569753d098ddb52ed
pinatflo <florian.pinat@utt.fr>
2013-05-15 16:15:42 +0300

    Update Translate.rb

--- spec/features/Translate.rb
+++ spec/features/Translate.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'Translation' do
 
-    scenario 'Add a translation' do
+    scenario 'Translation' do
           visit '/'
           click_on 'The plus'
           fill_in 'Title'
@@ -10,29 +10,15 @@ feature 'Translation' do
           page.should have_author 'Edgar Allan Poe'
           page.should have_traductor'Martin Dupont'
           fill_in 'blocs'
-    end
-
-    scenario 'Link blocs' do
-          visit '/'
-          page.should have_author 'Edgar Allan Poe'
-          page.should have_traductor'Martin Dupont'
-          page.should have_blocs 'une fois, sur le minuit lugubre ...'
-          page.should have_title 'Le corbeau'
-          page.should have_language 'Français'
           click_on 'link'
           page.should have_blocs 'une fois, sur le minuit lugubre ...  Sur maint précieux et curieux volume ...'
           page.should have_button 'cut'
-    end
-
-    scenario 'Edit a translation' do
-        visit '/'
-        page.should have_author 'Edgar Allan Poe'
-        page.should have_traductor'Martin Dupont'
-        page.should have_blocs 'une fois, sur le minuit lugubre ...'
-        page.should have_title 'Le corbeau'
-        page.should have_language 'Français'
-        click_on 'blocs'
-        edit 'blocs'
+          click_on 'blocs'
+          edit 'blocs'
+          click_on 'cut'
+          page.should have_bloc 'une fois, sur le minuit ...'
+          page.should have_bloc 'Sur maint précieux et curieux volume ...'
+          page.should have_button 'link'
     end
 
 end
