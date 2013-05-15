0df32e13ba18734f2d3ebdbc7179b5ad4ebcb37e
Paul Aubineau <aubineau.p@gmail.com>
2013-05-15 15:21:57 +0200

    Create compare_traduction.rb
    Acceptance testing of the navigation and translation comparation

---
+++ spec/features/compare_traduction.rb
@@ -0,0 +1,22 @@
+###
+#This acceptance test is based on the V2.0 prototype of Traduxio 
+#(http://traduxio.test.hypertopic.org/)
+#It will be possible to display more than two translation at the same time in the V2.1 prototype. 
+#This test should be updated in order to validate this new feature.   
+###
+
+require 'spec_helper'
+feature 'Navigate and compare translation' do
+	scenario 'navigate and compare translation' do
+		visit '/'
+		#Choose an original work
+		click_on 'The lamp (Fungi from Yuggoth, 6)'
+		#A translation is visible by default
+		page.should have_bloc 'We found the lamp inside those hollow cliffs...'
+		page.should have_bloc 'Nous trouvâmes la lampe à l\'intérieur de ces cavités rocheuses...'
+		#Open another translation
+		click_on 'François Truchaud'
+		page.should have_bloc 'We found the lamp inside those hollow cliffs...'
+		page.should have_bloc 'Nous trouvâmes la lampe à l’intérieur de ces falaises creuses...'
+	end
+end
