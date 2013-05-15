bb4f492c03e271964ccdf1521a9b0188c1bb5e9f
pinatflo <florian.pinat@utt.fr>
2013-05-15 15:36:39 +0200

    TEST : "update Translate.rb"

--- spec/features/Translate.rb
+++ spec/features/Translate.rb
@@ -11,13 +11,13 @@ feature 'Translation' do
           page.should have_traductor'Martin Dupont'
           fill_in 'blocs'
           click_on 'link'
-          page.should have_bloc 'une fois, sur le minuit lugubre ...  Sur maint précieux et curieux volume ...'
+          page.should have_content 'une fois, sur le minuit lugubre ...  Sur maint précieux et curieux volume ...'
           page.should have_button 'cut'
           click_on 'blocs'
           edit 'blocs'
           click_on 'cut'
-          page.should have_bloc 'une fois, sur le minuit ...'
-          page.should have_bloc 'Sur maint précieux et curieux volume ...'
+          page.should have_content 'une fois, sur le minuit ...'
+          page.should have_content 'Sur maint précieux et curieux volume ...'
           page.should have_button 'link'
     end
 
