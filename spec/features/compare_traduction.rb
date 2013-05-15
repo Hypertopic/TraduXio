e109e7d0363d192a18442df8920d443667b005a7
Paul Aubineau <aubineau.p@gmail.com>
2013-05-15 16:27:02 +0300

    Update compare_traduction.rb

--- spec/features/compare_traduction.rb
+++ spec/features/compare_traduction.rb
@@ -12,11 +12,11 @@ feature 'Navigate and compare translation' do
 		#Choose an original work
 		click_on 'The lamp (Fungi from Yuggoth, 6)'
 		#A translation is visible by default
-		page.should have_bloc 'We found the lamp inside those hollow cliffs...'
-		page.should have_bloc 'Nous trouvâmes la lampe à l\'intérieur de ces cavités rocheuses...'
+		page.should have_bloc 'We found the lamp inside those hollow cliffs'
+		page.should have_bloc 'Nous trouvâmes la lampe à l\'intérieur de ces cavités rocheuses'
 		#Open another translation
 		click_on 'François Truchaud'
-		page.should have_bloc 'We found the lamp inside those hollow cliffs...'
-		page.should have_bloc 'Nous trouvâmes la lampe à l’intérieur de ces falaises creuses...'
+		page.should have_bloc 'We found the lamp inside those hollow cliffs'
+		page.should have_bloc 'Nous trouvâmes la lampe à l’intérieur de ces falaises creuses'
 	end
 end
