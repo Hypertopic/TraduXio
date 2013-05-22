ce9a8932dcd0abf8071b480d5efeec7d0af3c78f
pinatflo <florian.pinat@utt.fr>
2013-05-22 16:51:55 +0300

    Update Translate.rb

--- spec/features/Translate.rb
+++ spec/features/Translate.rb
@@ -7,17 +7,14 @@ feature 'Translation' do
         click_on '+'
         fill_in 'title', :with => 'The Raven'
         fill_in 'author', :with => 'Edgar Allan Poe'
-        fill_in 'core', :with => '0nce upon a midnight dreary ... Over many a quaint and curious volume ... '
+        fill_in 'content1' :with => 'Once upon a midnight dreary, while I pondered, weak and weary,'
+        fill_in 'content2', :with => 'Over many a quaint and curious volume of forgotten lore'
         click_on 'Enregistrer'
     end
   
 
     scenario 'Translation' do
-          visit '/work/index'
-          click_on 'Edgar Allan Poe'
-          click_on 'The Raven'
-          
-          visit '/work/newread/id/41894#tr42023'
+
           
           click_on '+'
           fill_in 'Title', :with => 'Le Corbeau'
@@ -25,12 +22,12 @@ feature 'Translation' do
           page.should have_field  'Translator', :with => 'Martin Dupont'
           page.should have_field 'author', :with => 'Edgar Allan Poe'
           
-          fill_in 'content1' , :with => 'Une fois, sur le minuit lugubre ...'
-          fill_in 'content2' , :with => 'Sur maint précieux et curieux volume ...'
+          fill_in 'content1' , :with => 'Une fois, sur le minuit lugubre pendant que je méditais, faible et fatigué,'
+          fill_in 'content2' , :with => 'Sur maint précieux et curieux volume d une doctrine oubliée'
           
           click_on 'link'
           
-          page.should have_field 'content1' , :with => 'Une fois, sur le minuit lugubre ...  Sur maint précieux et curieux volume ...'
+          page.should have_field 'content1' , :with => 'Une fois, sur le minuit lugubre pendant que je méditais, faible et fatigué, Sur maint précieux et curieux volume d une doctrine oubliée'
           
           page.should_not have_field 'content2' 
           
@@ -41,8 +38,8 @@ feature 'Translation' do
           
           click_on 'cut_fields'
           
-          page.should have_field 'content1' , :with => 'une fois, sur le minuit lugubre ...'
-          page.should have_field 'content2' , :with => 'Sur maint précieux et curieux volume ...'
+          page.should have_field 'content1' , :with => 'Une fois, sur le minuit lugubre pendant que je méditais, faible et fatigué,'
+          page.should have_field 'content2' , :with => 'Sur maint précieux et curieux volume d une doctrine oubliée'
           
           page.should have_button 'link_fields'
     end
