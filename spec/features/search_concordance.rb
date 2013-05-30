653340619b682a1714916188ac56241d1fb83a90
Jeremie JALOUZET <jeremie.jalouzet@utt.fr>
2013-05-30 13:49:49 +0200

    FIX : Syntax errors in a test

--- spec/features/search_concordance.rb
+++ spec/features/search_concordance.rb
@@ -5,9 +5,9 @@ feature 'Search for a concordance' do
 	scenario 'Research a valid sequence of words' do
 		visit '/'
 		click_on 'The lamp (Fungi from Yuggoth, 6)'
-		fill_in 'Rechercher', :with => 'the ancient oil'
+		fill_in 'query', :with => 'the ancient oil'
 		click_on 'Rechercher'
-		in_bold().should equal 'the ancient oil'
+		page.should have_css('b', :text => 'the ancient oil')
 		page.should have_content 'Trad. François Truchaud'
 		page.should have_content 'Trad. Aurélien Bénel'
 	end
@@ -15,7 +15,7 @@ feature 'Search for a concordance' do
 	scenario 'Research a sequence of words in the wrong order' do
 		visit '/'
 		click_on 'The lamp (Fungi from Yuggoth, 6)'
-		fill_in 'Rechercher', :with => 'ancient the'
+		fill_in 'query', :with => 'ancient the'
 		click_on 'Rechercher'
 		page.should_not have_content 'Trad. François Truchaud'
 		page.should_not have_content 'Trad. Aurélien Bénel'
@@ -24,9 +24,9 @@ feature 'Search for a concordance' do
 	scenario 'Research a beginning of a word' do
 		visit '/'
 		click_on 'The lamp (Fungi from Yuggoth, 6)'
-		fill_in 'Rechercher', :with => 'anc'
+		fill_in 'query', :with => 'anc'
 		click_on 'Rechercher'
-		in_bold().should equal 'anc'
+		page.should have_css('b', :text => 'anc')
 		page.should have_content 'Trad. François Truchaud'
 		page.should have_content 'Trad. Aurélien Bénel'
 	end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,13 +7,13 @@ Capybara.javascript_driver = :webkit
 Capybara.app_host = 'http://traduxio.test.hypertopic.org/'
 
 def a_string()
-  s = ('a'..'z').to_a.shuffle[0,8].join
+	s = ('a'..'z').to_a.shuffle[0,8].join
 end
 
 def prefer_language(language)
-  page.driver.header 'Accept-Language', language
+	page.driver.header 'Accept-Language', language
 end
 
 def in_bold()
-  page.all('b', :visible => "true")
+	page.all(:css, "b")
 end
