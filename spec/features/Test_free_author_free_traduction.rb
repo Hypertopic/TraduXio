5045698454f200ecd21b123461ddbe40d51af6d3
morechri <more.christophe@gmail.com>
2013-06-05 15:32:03 +0200

    Update Test_free_author_free_traduction.rb
    Compare only the first line to avoid jump line encoding problems
    Replace '' by \'
    Replace "page should" by "page.should"

--- spec/features/Test_free_author_free_traduction.rb
+++ spec/features/Test_free_author_free_traduction.rb
@@ -12,34 +12,12 @@ feature 'copyright' do
 		  
 		  #/** LOADING: REDIRECTING TO THE TEXT PAGE **/
 		  
-		  page should have_content 'We found the lamp inside those hollow cliffs
-									Whose chiseled sign no priest in Thebes could read
-									And from whose caverns frightened hieroglyphs
-									Warned every living creature of earth''s breed.
-									No more was there - just that one brazen bowl
-									With traces of a curious oil within;
-									Fretted with some obscurely patterned scroll,
-									And symbols hinting vaguely of strange sin.
-										
-									Little the fears of forty centuries meant
-									To us as we bore off our slender spoil,
-									And when we scanned it in our darkened tent
-									We struck a match to test the ancient oil.
-									It blazed - great God!... But the vast shapes we saw
-									In that mad flash have seared our lives with awe.'
+		  page.should have_content 'We found the lamp inside those hollow cliffs'
 									
 									
-		page should have_content '  Nous trouvâmes la lampe à l''intérieur de ces cavités rocheuses
-									Aux signes sculptés qu''aucun prêtre de Thèbes ne déchiffra jamais
-									Et dont les hiéroglyphes effrayés de leurs cavernes
-									avertissaient toute créature vivante engendrée par la terre. 
-									Insignifiantes étaient les peurs de quarante siècles
-									à nos yeux alors que nous emportions notre maigre butin
-									Et en l''examinant dans l''obscurité de nos tentes,
-									nous frottâmes une allumette pour tester l''ancienne huile.
-									Elle flamba - grand Dieu ! ... Mais les vastes'	
+		page.should have_content 'Nous trouvâmes la lampe à l\'intérieur de ces cavités rocheuses'	
 
-		page should_not have_link 'François Truchaud'
+		page.should_not have_link 'François Truchaud'
     end
 
 end
