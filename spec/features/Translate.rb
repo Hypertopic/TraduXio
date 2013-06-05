d174433309099d433bde84a7f335a720bdda1ceb
morechri <more.christophe@gmail.com>
2013-06-05 16:39:39 +0300

    Update Translate.rb
    Replace "content n" by "originalBlock n" for the original text blocks
    Replace "content n" by "translationBlock n" for the translation blocks

--- spec/features/Translate.rb
+++ spec/features/Translate.rb
@@ -7,9 +7,9 @@ feature 'Translation' do
         click_on '+'
         fill_in 'title', :with => 'The Raven'
         fill_in 'author', :with => 'Edgar Allan Poe'
-        fill_in 'content1', :with => 'Once upon a midnight dreary, while I pondered, weak and weary,'
-        fill_in 'content2', :with => 'Over many a quaint and curious volume of forgotten lore'
-        click_on 'Enregistrer'
+        fill_in 'originalBlock1', :with => 'Once upon a midnight dreary, while I pondered, weak and weary,'
+        fill_in 'originalBlock2', :with => 'Over many a quaint and curious volume of forgotten lore'
+        click_on 'Save'
     end
   
 
@@ -22,24 +22,24 @@ feature 'Translation' do
           page.should have_field  'Translator', :with => 'Martin Dupont'
           page.should have_field 'author', :with => 'Edgar Allan Poe'
           
-          fill_in 'content1' , :with => 'Une fois, sur le minuit lugubre pendant que je méditais, faible et fatigué,'
-          fill_in 'content2' , :with => 'Sur maint précieux et curieux volume d une doctrine oubliée'
+          fill_in 'translationBlock1' , :with => 'Une fois, sur le minuit lugubre pendant que je méditais, faible et fatigué,'
+          fill_in 'translationBlock2' , :with => 'Sur maint précieux et curieux volume d une doctrine oubliée'
           
           click_on 'link'
           
-          page.should have_field 'content1' , :with => 'Une fois, sur le minuit lugubre pendant que je méditais, faible et fatigué, Sur maint précieux et curieux volume d une doctrine oubliée'
+          page.should have_field 'translationBlock1' , :with => 'Une fois, sur le minuit lugubre pendant que je méditais, faible et fatigué, Sur maint précieux et curieux volume d une doctrine oubliée'
           
-          page.should_not have_field 'content2' 
+          page.should_not have_field 'translationBlock2' 
           
           page.should have_button 'cut_fields'
           
-          click_on 'content1'
-          fill_in 'content1'
+          click_on 'translationBlock1'
+          fill_in 'translationBlock1'
           
           click_on 'cut_fields'
           
-          page.should have_field 'content1' , :with => 'Une fois, sur le minuit lugubre pendant que je méditais, faible et fatigué,'
-          page.should have_field 'content2' , :with => 'Sur maint précieux et curieux volume d une doctrine oubliée'
+          page.should have_field 'translationBlock1' , :with => 'Une fois, sur le minuit lugubre pendant que je méditais, faible et fatigué,'
+          page.should have_field 'translationBlock2' , :with => 'Sur maint précieux et curieux volume d une doctrine oubliée'
           
           page.should have_button 'link_fields'
     end 
