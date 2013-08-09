c19e972c298dcdc9fa0e29722b5d5a3c8e8d5fb2
Aurelien Benel <aurelien.benel@utt.fr>
2013-08-09 22:26:46 +0200

    FEATURE: Display multiple aligned translations of a work.

--- shows/work.js
+++ shows/work.js
@@ -2,43 +2,64 @@ function(o, req) {
   // !json templates.work
   // !code lib/mustache.js
 
-  const NEW_LINE = /\n/g;
-  function toHtml(string) {
-    return string.replace(NEW_LINE, "<br/>");
+  function isJoined(texts, n) {
+    for each (var version in texts) {
+      if (version[n]==null) {
+        return true;
+      }
+    }
+    return false;
   }
 
-  const CURRENT = (req.query.translation)? req.query.translation
-    : Object.keys(o.translations)[0];
-  const TRANSLATION = o.translations[CURRENT].text;
+  const NEW_LINE = /\n/g;
+
+  function toHtml(string) {
+    return (string)? string.replace(NEW_LINE, "<br/>") : null;
+  }
+
+  function getRawUnit(texts, n) {
+    var result = [];
+    for each (var version in texts) {
+      result.push(toHtml(version[n]));
+    }
+    return result;
+  }
+
+  function getVersions(texts, n) {
+    const LENGTH = texts[0].length;
+    var block = getRawUnit(texts, n);
+    while (++n<LENGTH && isJoined(texts, n)) {
+      var raw =  getRawUnit(texts, n);
+      for (var version in raw) {
+        if (raw[version]) {
+          block[version] += "<br/>" + raw[version];
+          log(raw[version]);
+        }
+      }
+    }
+    return {
+      versions: block, 
+      next: (n<LENGTH)? n : null
+    };
+  }
+
   var data = {
     title: o.title,
     creator: o.creator,
     language: o.language,
-    current: CURRENT,
-    others: [],
     units: []
   };
-  for (var t in o.translations) {
-    if (t!=CURRENT) {
-      data.others.push(t);
-    }
+  var texts = [];
+  if (o.text) {
+    texts.push(o.text);
   }
-  var start = 0;
-  var sources = [];
-  for (var i = 0; i<TRANSLATION.length; i++) {
-    if (TRANSLATION[i]!=null && i>start) {
-      data.units.push({
-        sources: sources,
-        target: toHtml(TRANSLATION[start])
-      });
-      start = i;
-      sources = [];
-    }
-    sources.push(toHtml(o.text[i]));
+  for each (var t in o.translations) {
+    texts.push(t.text);
   }
-  data.units.push({
-    sources: sources,
-    target: toHtml(TRANSLATION[start])
-  });
+  var block = {next: 0};
+  do {
+    block = getVersions(texts, block.next);
+    data.units.push({versions: block.versions});
+  } while (block.next);
   return Mustache.to_html(templates.work, data);
 }
