1d6f47d957bd21d02ccf24046688045f5c53c981
Aurelien Benel <aurelien.benel@utt.fr>
2013-08-09 22:28:24 +0200

    FIX: Access control disabled for v2.0.

--- views/works/map.js
+++ views/works/map.js
@@ -1,10 +1,3 @@
 function (o) {
-  if (o.creativeCommons) {
-    for each (var t in o.translations) {
-      if (t.creativeCommons) {
-        emit([o.language, o.creator], o.title);
-        break;
-      }
-    }
-  }
+  emit([o.language, o.creator], o.title);
 }
