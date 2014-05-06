bc73f8038f8de4074a7850352f2773a1d7374de2
Alexis Deprez <deprez.alexis@laposte.net>
2014-05-06 19:47:07 +0200

    Revert "REFACTOR: use works file for licence updates"
    This reverts commit 19e457c36f64442b6585e00611588a4fc1a99e6c.

--- _attachments/work.js
+++ _attachments/work.js
@@ -1,4 +1,4 @@
-﻿
+
   $.fn.toggleName = function(name1, name2) {
     this.val(
       (this.val()==name1)? name2 : name1
@@ -243,7 +243,7 @@
 	  container.find("select").remove();
 	  target.removeClass("edit").show();
 	} else {
-	  var language = $(getLanguageSelector());
+	  var language = $("#header").find("#language").clone();
 	  language.val(target.data("id"));
 	  language.addClass("editedMeta").css("width", "50%");
 	  language.on("change", function() {
@@ -255,9 +255,9 @@
 		  contentType: 'text/plain',
 		  data: JSON.stringify({"key":"language", "value": language.val()})
 		}).done(function() {
-		  var lang = language.find("option:selected");
-		  var lang_id = lang.val();
-		  var lang_text = lang.text().split("-")[0];
+		  var lang = language.find("option:selected").text();
+		  var lang_id = lang.substring(0, 2);
+		  var lang_text = lang.substring(5);
 		  target.data("id", lang_id).attr("data-id", lang_id);
 		  target.text(lang_text);
 		  $("#hexapla").find(".close[data-version='" + ref + "']").find(".language")
@@ -269,18 +269,17 @@
 	}
   }
   
-  function setEditState(isEdited, container, name) {
-	setEditStateForComponent(isEdited, container, name, "focusout", '<textarea class="editedMeta ' + name + '" />');
+  function setEditState(isEdited, container, name, auto) {
+	setEditStateForComponent(isEdited, container, name, "focusout", '<textarea class="editedMeta ' + name + '" />', auto);
   }
   
-  function setEditStateForComponent(isEdited, container, name, event, textComponent) {
+  function setEditStateForComponent(isEdited, container, name, event, textComponent, auto) {
 	var target = container.find("." + name);
 	if(isEdited) {
 	  target.removeClass("edit").show();
 	} else {
 	  target.addClass("edit");
 	  var component=$(textComponent);
-	  component.attr("placeholder", name);
 	  component.on(event, function() {
 		if(component.hasClass("dirty")) {
 		  var id = $("#hexapla").data("id");
@@ -330,198 +329,9 @@
 	return false;
   }
   
-  function getLanguageSelector() {
-	return '<select>'+ '<option value="﻿aa">Afar - Afaraf - Afar</option>'
-	+ '<option value="ab">Abkhaze - Аҧсуа - Abkhazian</option>'
-	+ '<option value="ae">Avestique - Avesta - Avestan</option>'
-	+ '<option value="af">Afrikaans - Afrikaans - Afrikaans</option>'
-	+ '<option value="ak">Akan - Akan - Akan</option>'
-	+ '<option value="am">Amharique - አማርኛ - Amharic</option>'
-	+ '<option value="an">Aragonais - Aragonés - Aragonese</option>'
-	+ '<option value="ar">Arabe - ‫العربية - Arabic</option>'
-	+ '<option value="as">Assamais - অসমীয়া - Assamese</option>'
-	+ '<option value="av">Avar - авар мацӀ - Avaric</option>'
-	+ '<option value="ay">Aymara - Aymar aru - Aymara</option>'
-	+ '<option value="az">Azéri - Azərbaycan dili - Azerbaijani</option>'
-	+ '<option value="ba">Bachkir - башҡорт теле - Bashkir</option>'
-	+ '<option value="be">Biélorusse - Беларуская - Belarusian</option>'
-	+ '<option value="bg">Bulgare - български език - Bulgarian</option>'
-	+ '<option value="bh">Bihari - भोजपुरी - Bihari</option>'
-	+ '<option value="bi">Bichelamar - Bislama - Bislama</option>'
-	+ '<option value="bm">Bambara - Bamanankan - Bambara</option>'
-	+ '<option value="bn">Bengalî - বাংলা - Bengali</option>'
-	+ '<option value="bo">Tibétain - བོད་ཡིག - Tibetan</option>'
-	+ '<option value="br">Breton - Brezhoneg - Breton</option>'
-	+ '<option value="bs">Bosnien - Bosanski jezik - Bosnian</option>'
-	+ '<option value="ca">Catalan - Català - Catalan</option>'
-	+ '<option value="ce">Tchétchène - нохчийн мотт - Chechen</option>'
-	+ '<option value="ch">Chamorro - Chamoru - Chamorro</option>'
-	+ '<option value="co">Corse - Corsu - Corsican</option>'
-	+ '<option value="cr">Cri - ᓀᐦᐃᔭᐍᐏᐣ - Cree</option>'
-	+ '<option value="cs">Tchèque - Česky - Czech</option>'
-	+ '<option value="cu">Vieux slave - Словѣньскъ - Old Church Slavonic</option>'
-	+ '<option value="cv">Tchouvache - чӑваш чӗлхи - Chuvash</option>'
-	+ '<option value="cy">Gallois - Cymraeg - Welsh</option>'
-	+ '<option value="da">Danois - Dansk - Danish</option>'
-	+ '<option value="de">Allemand - Deutsch - German</option>'
-	+ '<option value="dv">Dhivehi - ‫ދިވެހި - Divehi</option>'
-	+ '<option value="dz">Dzongkha - རྫོང་ཁ - Dzongkha</option>'
-	+ '<option value="ee">Ewe - Ɛʋɛgbɛ - Ewe</option>'
-	+ '<option value="el">Grec moderne - Ελληνικά - Greek</option>'
-	+ '<option value="en">Anglais - English - English</option>'
-	+ '<option value="eo">Espéranto - Esperanto - Esperanto</option>'
-	+ '<option value="es">Espagnol - "Español; castellano" - Spanish</option>'
-	+ '<option value="et">Estonien - Eesti keel - Estonian</option>'
-	+ '<option value="eu">Basque - Euskara - Basque</option>'
-	+ '<option value="fa">Persan - ‫فارسی - Persian</option>'
-	+ '<option value="ff">Peul - Fulfulde - Fulah</option>'
-	+ '<option value="fi">Finnois - Suomen kieli - Finnish</option>'
-	+ '<option value="fj">Fidjien - Vosa Vakaviti - Fijian</option>'
-	+ '<option value="fo">Féringien - Føroyskt - Faroese</option>'
-	+ '<option value="fr">Français - French</option>'
-	+ '<option value="fy">Frison - Frysk - Western Frisian</option>'
-	+ '<option value="ga">Irlandais - Gaeilge - Irish</option>'
-	+ '<option value="gd">Écossais - Gàidhlig - Scottish Gaelic</option>'
-	+ '<option value="gl">Galicien - Galego - Galician</option>'
-	+ '<option value="gn">Guarani - Avañe\'ẽ - Guarani</option>'
-	+ '<option value="gu">Gujarâtî - ગુજરાતી - Gujarati</option>'
-	+ '<option value="gv">Mannois - Ghaelg - Manx</option>'
-	+ '<option value="ha">Haoussa - ‫هَوُسَ - Hausa</option>'
-	+ '<option value="he">Hébreu - ‫עברית - Hebrew</option>'
-	+ '<option value="hi">Hindî - "हिन्दी ; हिंदी" - Hindi</option>'
-	+ '<option value="ho">Hiri motu - Hiri Motu - Hiri Motu</option>'
-	+ '<option value="hr">Croate - Hrvatski - Croatian</option>'
-	+ '<option value="ht">Créole haïtien - Kreyòl ayisyen - Haitian</option>'
-	+ '<option value="hu">Hongrois - magyar - Hungarian</option>'
-	+ '<option value="hy">Arménien - Հայերեն - Armenian</option>'
-	+ '<option value="hz">Herero - Otjiherero - Herero</option>'
-	+ '<option value="ia">Interlingua - Interlingua - Interlingua</option>'
-	+ '<option value="id">Indonésien - Bahasa Indonesia - Indonesian</option>'
-	+ '<option value="ie">Occidental - Interlingue - Interlingue</option>'
-	+ '<option value="ig">Igbo - Igbo - Igbo</option>'
-	+ '<option value="ii">Yi - ꆇꉙ - Sichuan Yi</option>'
-	+ '<option value="ik">Inupiaq - Iñupiaq - Inupiaq</option>'
-	+ '<option value="io">Ido - Ido - Ido</option>'
-	+ '<option value="is">Islandais - Íslenska - Icelandic</option>'
-	+ '<option value="it">Italien - Italiano - Italian</option>'
-	+ '<option value="iu">Inuktitut - ᐃᓄᒃᑎᑐᑦ - Inuktitut</option>'
-	+ '<option value="ja">Japonais - 日本語 (にほんご) - Japanese</option>'
-	+ '<option value="jv">Javanais - Basa Jawa - Javanese</option>'
-	+ '<option value="ka">Géorgien - ქართული - Georgian</option>'
-	+ '<option value="kg">Kikongo - KiKongo - Kongo</option>'
-	+ '<option value="ki">Kikuyu - Gĩkũyũ - Kikuyu</option>'
-	+ '<option value="kj">Kuanyama - Kuanyama - Kwanyama</option>'
-	+ '<option value="kk">Kazakh - Қазақ тілі - Kazakh</option>'
-	+ '<option value="kl">Kalaallisut - Kalaallisut - Kalaallisut</option>'
-	+ '<option value="km">Khmer - ភាសាខ្មែរ - Khmer</option>'
-	+ '<option value="kn">Kannara - ಕನ್ನಡ - Kannada</option>'
-	+ '<option value="ko">Coréen - 한국어 (韓國語) - Korean</option>'
-	+ '<option value="kr">Kanouri - Kanuri - Kanuri</option>'
-	+ '<option value="ks">Kashmiri - "कश्मीरी ; كشميري" - Kashmiri</option>'
-	+ '<option value="ku">Kurde - كوردی - Kurdish</option>'
-	+ '<option value="kv">Komi - коми кыв - Komi</option>'
-	+ '<option value="kw">Cornique - Kernewek - Cornish</option>'
-	+ '<option value="ky">Kirghiz - кыргыз тили - Kirghiz</option>'
-	+ '<option value="la">Latin - "Latine ; lingua latina" - Latin</option>'
-	+ '<option value="lb">Luxembourgeois - Lëtzebuergesch - Luxembourgish</option>'
-	+ '<option value="lg">Ganda - Luganda - Ganda</option>'
-	+ '<option value="li">Limbourgeois - Limburgs - Limburgish</option>'
-	+ '<option value="ln">Lingala - Lingála - Lingala</option>'
-	+ '<option value="lo">Lao - ພາສາລາວ - Lao</option>'
-	+ '<option value="lt">Lituanien - Lietuvių kalba - Lithuanian</option>'
-	+ '<option value="lu">Luba-katanga - kiluba - Luba-Katanga</option>'
-	+ '<option value="lv">Letton - Latviešu valoda - Latvian</option>'
-	+ '<option value="mg">Malgache - Fiteny malagasy - Malagasy</option>'
-	+ '<option value="mh">Marshallais - Kajin M̧ajeļ - Marshallese</option>'
-	+ '<option value="mi">Māori de Nouvelle-Zélande - Te reo Māori - Māori</option>'
-	+ '<option value="mk">Macédonien - македонски јазик - Macedonian</option>'
-	+ '<option value="ml">Malayalam - മലയാളം - Malayalam</option>'
-	+ '<option value="mn">Mongol - Монгол - Mongolian</option>'
-	+ '<option value="mo">Moldave - лимба молдовеняскэ - Moldavian</option>'
-	+ '<option value="mr">Marâthî - मराठी - Marathi</option>'
-	+ '<option value="ms">Malais - بهاس ملايو - Malay</option>'
-	+ '<option value="mt">Maltais - Malti - Maltese</option>'
-	+ '<option value="my">Birman - ဗမာစာ - Burmese</option>'
-	+ '<option value="na">Nauruan - Ekakairũ Naoero - Nauru</option>'
-	+ '<option value="nb">Norvégien Bokmål - Norsk bokmål - Norwegian Bokmål</option>'
-	+ '<option value="nd">Ndébélé du Nord - isiNdebele - North Ndebele</option>'
-	+ '<option value="ne">Népalais - नेपाली - Nepali</option>'
-	+ '<option value="ng">Ndonga - Owambo - Ndonga</option>'
-	+ '<option value="nl">Néerlandais - Nederlands - Dutch</option>'
-	+ '<option value="nn">Norvégien Nynorsk - Norsk nynorsk - Norwegian Nynorsk</option>'
-	+ '<option value="no">Norvégien - Norsk - Norwegian</option>'
-	+ '<option value="nr">Ndébélé du Sud - Ndébélé - South Ndebele</option>'
-	+ '<option value="nv">Navajo - "Diné bizaad ; Dinékʼehǰí" - Navajo</option>'
-	+ '<option value="ny">Chichewa - "ChiCheŵa ; chinyanja" - Chichewa</option>'
-	+ '<option value="oc">Occitan - Occitan - Occitan</option>'
-	+ '<option value="oj">Ojibwé - ᐊᓂᔑᓈᐯᒧᐎᓐ - Ojibwa</option>'
-	+ '<option value="om">Oromo - Afaan Oromoo - Oromo</option>'
-	+ '<option value="or">Oriya - ଓଡ଼ିଆ - Oriya</option>'
-	+ '<option value="os">Ossète - Ирон æвзаг - Ossetian</option>'
-	+ '<option value="pa">Panjâbî - "ਪੰਜਾਬੀ ; پنجابی" - Panjabi</option>'
-	+ '<option value="pi">Pâli - पािऴ - Pāli</option>'
-	+ '<option value="pl">Polonais - Polski - Polish</option>'
-	+ '<option value="ps">Pachto - ‫پښتو - Pashto</option>'
-	+ '<option value="pt">Portugais - Português - Portuguese</option>'
-	+ '<option value="qu">Quechua - "Runa Simi ; Kichwa" - Quechua</option>'
-	+ '<option value="rm">Romanche - Rumantsch grischun - Romansh</option>'
-	+ '<option value="rn">Kirundi - kiRundi - Kirundi</option>'
-	+ '<option value="ro">Roumain - Română - Romanian</option>'
-	+ '<option value="ru">Russe - русский язык - Russian</option>'
-	+ '<option value="rw">Kinyarwanda - Kinyarwanda - Kinyarwanda</option>'
-	+ '<option value="sa">Sanskrit - संस्कृतम् - Sanskrit</option>'
-	+ '<option value="sc">Sarde - sardu - Sardinian</option>'
-	+ '<option value="sd">Sindhi - "सिन्धी ; ‫سنڌي، سندھی" - Sindhi</option>'
-	+ '<option value="se">Same du Nord - Davvisámegiella - Northern Sami</option>'
-	+ '<option value="sg">Sango - Yângâ tî sängö - Sango</option>'
-	+ '<option value="si">Cingalais - සිංහල - Sinhalese</option>'
-	+ '<option value="sk">Slovaque - Slovenčina - Slovak</option>'
-	+ '<option value="sl">Slovène - Slovenščina - Slovene</option>'
-	+ '<option value="sm">Samoan - Gagana fa\'a Samoa - Samoan</option>'
-	+ '<option value="sn">Shona - chiShona - Shona</option>'
-	+ '<option value="so">Somali - Soomaaliga - Somali</option>'
-	+ '<option value="sq">Albanais - Shqip - Albanian</option>'
-	+ '<option value="sr">Serbe - српски језик - Serbian</option>'
-	+ '<option value="ss">Siswati - SiSwati - Swati</option>'
-	+ '<option value="st">Sotho du Sud - seSotho - Sotho</option>'
-	+ '<option value="su">Soundanais - Basa Sunda - Sundanese</option>'
-	+ '<option value="sv">Suédois - Svenska - Swedish</option>'
-	+ '<option value="sw">Swahili - Kiswahili - Swahili</option>'
-	+ '<option value="ta">Tamoul - தமிழ் - Tamil</option>'
-	+ '<option value="te">Télougou - తెలుగు - Telugu</option>'
-	+ '<option value="tg">Tadjik - "тоҷикӣ ; toğikī ; ‫تاجیکی" - Tajik</option>'
-	+ '<option value="th">Thaï - ไทย - Thai</option>'
-	+ '<option value="ti">Tigrinya - ትግርኛ - Tigrinya</option>'
-	+ '<option value="tk">Turkmène - "Türkmen ; Түркмен" - Turkmen</option>'
-	+ '<option value="tl">Tagalog - Tagalog - Tagalog</option>'
-	+ '<option value="tn">Tswana - seTswana - Tswana</option>'
-	+ '<option value="to">Tongien - faka Tonga - Tonga</option>'
-	+ '<option value="tr">Turc - Türkçe - Turkish</option>'
-	+ '<option value="ts">Tsonga - xiTsonga - Tsonga</option>'
-	+ '<option value="tt">Tatar - "татарча ; tatarça ; ‫تاتارچا" - Tatar</option>'
-	+ '<option value="tw">Twi - Twi - Twi</option>'
-	+ '<option value="ty">Tahitien - Reo Mā`ohi - Tahitian</option>'
-	+ '<option value="ug">Ouïghour - "Uyƣurqə ; ‫ئۇيغۇرچ" - Uighur</option>'
-	+ '<option value="uk">Ukrainien - українська мова - Ukrainian</option>'
-	+ '<option value="ur">Ourdou - ‫اردو - Urdu</option>'
-	+ '<option value="uz">Ouzbek - Ўзбек - Uzbek</option>'
-	+ '<option value="ve">Venda - tshiVenḓa - Venda</option>'
-	+ '<option value="vi">Vietnamien - Tiếng Việt - Viêt Namese</option>'
-	+ '<option value="vo">Volapük - Volapük - Volapük</option>'
-	+ '<option value="wa">Wallon - Walon - Walloon</option>'
-	+ '<option value="wo">Wolof - Wollof - Wolof</option>'
-	+ '<option value="xh">Xhosa - isiXhosa - Xhosa</option>'
-	+ '<option value="yi">Yiddish - ‫ייִדיש - Yiddish</option>'
-	+ '<option value="yo">Yoruba - Yorùbá - Yoruba</option>'
-	+ '<option value="za">Zhuang - Saɯ cueŋƅ - Zhuang</option>'
-	+ '<option value="zh">Chinois - 中文, 汉语, 漢語 - Chinese</option>'
-	+ '<option value="zu">Zoulou - isiZulu - Zulu</option>'
-	+ '</select>';
-  }
-  
   function clickDeleteVersion() {
 	var ref = $(this).closest("th").data("version");
-	if(confirm('Supprimer la traduction "' + ref + '" ?')) {
+	if(confirm("Supprimer la traduction de " + ref + " ?")) {
 	  deleteVersion(ref);
 	}
   }
