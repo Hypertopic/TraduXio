da431a142522313f514e506a3c702a0f340d34ad
Alexis Deprez <deprez.alexis@laposte.net>
2014-05-12 10:47:10 +0200

    FEATURE: vary and animate quote (closes #69)

--- lists/works.js
+++ lists/works.js
@@ -37,29 +37,11 @@
       name: row.value
     });
   }
-  var quote;
-  var author;
-  switch(Math.floor(Math.random() * 3)) {
-	case 0:
-	  quote = "Du point de vue de la communauté, le langage n'est pas seulement un fait social, "
-		+ "[...] il est par l'altérité [...] le fondement de toute association humaine.";
-	  author = "Eugenio Coseriu";
-	  break;
-	case 1:
-	  quote = "La traduction révèle alors le texte à lui-même : en quelque sorte, le texte semble inachevé tant qu'il n'est pas traduit.";
-	  author = "François Rastier";
-	  break;
-	default:
-	  quote = "Une langue est un filet jeté sur la réalité des choses. Une autre langue est un autre filet. Il est rare que les mailles coïncident.";
-	  author = "Maurice Carrez";
-	  break;
-  }
   languageData.authors.push(authorData);
-  data.quote = quote;
-  data.author = author;
   data.languages.push(languageData);
   data.name="works";
   data.scripts=["ul-close"];
+  data.script=true;
   data.css=true;
   data.prefix=getPrefix(req.requested_path,0);
   return Mustache.to_html(this.templates.works, data,this.templates.partials);
