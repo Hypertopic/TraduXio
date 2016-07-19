0ad7870e36a342d73314b1f423015fc1e4978c93
Franck Eyraud <franck+github@yrnm.net>
2016-07-19 16:57:16 +0200

    FIX: Data loading in external bash file

---
+++ spec/load_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DATABASE=${1-http://localhost:5984/traduxio}
+
+cd $(dirname "$0")
+for i in samples/*.json; do 
+  curl -XPOST -H "Content-Type: application/json" -d@$i $DATABASE
+done
