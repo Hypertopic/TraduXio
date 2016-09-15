bf968da4256e99dcb7601877371e335364a5b2c5
Franck Eyraud <franck+github@yrnm.net>
2016-09-15 16:38:04 +0200

    TESTS: Code rearrangement.

--- spec/translation_helper.rb
+++ spec/translation_helper.rb
@@ -1,24 +1,41 @@
-def check_translation_metadata(metadata)
-  translation=find_open_translation metadata[:author]
-  expect(translation).to have_metadata(:date,metadata[:date]) if metadata.has_key?(:date)
-  expect(translation).to have_metadata(:title,metadata[:title]) if metadata.has_key?(:title)
-  expect(translation).to have_metadata(:creator,metadata[:author]) if metadata.has_key?(:author)
-  expect(translation).to have_metadata(:'work-creator',metadata[:creator]) if metadata.has_key?(:creator)
-  expect(translation).to have_metadata(:language,metadata[:language]) if metadata.has_key?(:language)
+def create_translation(version)
+  debug "click on add version button"
+  page.find("a#addVersion").trigger(:click)
+  debug "fill the creator #{version}"
+  fill_in :'work-creator', :with => version
+  begin
+    debug "click on create button"
+    #why do we need to click twice ????
+    page.find('input[name=do-create]').click
+    page.find('input[name=do-create]').click
+    debug "wait #{version} to appear"
+  end until has_translation?(version)
+  debug "created #{version}"
 end
 
-def fill_translation_text (author,number)
-  edit_translation author
-
-  text = []
-
-  number.times do |i|
-    block=random_text(1)
-    fill_block(author,i,block)
-    text[i]=block
+def edit_translation_metadata(version,options)
+  raise "Must pass a hash" if not options.is_a?(Hash)
+  previously_in_edit_mode=edit_translation version
+  edited=false
+  within (selvers(version,"thead.header th.pleat.open")) do
+    edited=fill_field(:date,options[:date]) if options.has_key?(:date)
+    edited=fill_field(:title,options[:title]) if options.has_key?(:title)
+    edited=fill_select(:language,options[:language]) if options.has_key?(:language)
+    if version!=:original
+      edited=fill_field(:'work-creator',options[:creator]) if options.has_key?(:creator)
+      edited=fill_field(:creator,options[:author]) if options.has_key?(:author)
+    else
+      edited=fill_field(:'work-creator',options[:author]) if options.has_key?(:author)
+    end
   end
-
-  text
+  if edited then
+    debug :blur
+    edited.trigger(:blur)
+  end
+  if options.has_key?(:author)
+    version=options[:author]
+  end
+  read_translation version if not previously_in_edit_mode
 end
 
 def random_translation_metadata
@@ -37,6 +54,27 @@ def create_random_translation
   read_translation data[:author]
   debug data
   data
+end
+
+def check_translation_metadata(metadata)
+  translation=find_open_translation metadata[:author]
+  expect(translation).to have_metadata(:date,metadata[:date]) if metadata.has_key?(:date)
+  expect(translation).to have_metadata(:title,metadata[:title]) if metadata.has_key?(:title)
+  expect(translation).to have_metadata(:creator,metadata[:author]) if metadata.has_key?(:author)
+  expect(translation).to have_metadata(:'work-creator',metadata[:creator]) if metadata.has_key?(:creator)
+  expect(translation).to have_metadata(:language,metadata[:language]) if metadata.has_key?(:language)
+end
+
+def fill_translation_text (author,number)
+  edit_translation author
+
+  text = random_text(1)
+
+  text.each_with_index { |paragraph, index|
+    fill_block(author,index,paragraph)
+  }
+
+  text
 end
 
 def getVersions
@@ -89,6 +127,19 @@ def toggle_translation(version)
   find_open_translation(version).find("input.edit").click
 end
 
+def open_translation(version)
+  if not is_open?(version)
+    debug "open translation #{version}"
+    find_translation(version).find("span.button.show").click
+  end
+end
+
+def close_translation(version)
+  if is_open?(version)
+    find_translation(version).find("span.button.hide").click
+  end
+end
+
 def read_translation(version)
   read=!is_edited?(version)
   if not read
@@ -115,19 +166,6 @@ def is_edited?(version)
   find_translation(version)[:class].include?("edit")
 end
 
-def open_translation(version)
-  if not is_open?(version)
-    debug "open translation #{version}"
-    find_translation(version).find("span.button.show").click
-  end
-end
-
-def close_translation(version)
-  if is_open?(version)
-    find_translation(version).find("span.button.hide").click
-  end
-end
-
 def delete_translation(version)
   open_translation version
   edit_translation version
@@ -136,44 +174,4 @@ def delete_translation(version)
   find_open_translation(version).find("span.delete").click
   debug "confirm deletion if #{version}"
   accept_alert
-end
-
-def edit_translation_metadata(version,options)
-  raise "Must pass a hash" if not options.is_a?(Hash)
-  previously_in_edit_mode=edit_translation version
-  edited=false
-  within (selvers(version,"thead.header th.pleat.open")) do
-    edited=fill_field(:date,options[:date]) if options.has_key?(:date)
-    edited=fill_field(:title,options[:title]) if options.has_key?(:title)
-    edited=fill_select(:language,options[:language]) if options.has_key?(:language)
-    if version!=:original
-      edited=fill_field(:'work-creator',options[:creator]) if options.has_key?(:creator)
-      edited=fill_field(:creator,options[:author]) if options.has_key?(:author)
-    else
-      edited=fill_field(:'work-creator',options[:author]) if options.has_key?(:author)
-    end
-  end
-  if edited then
-    debug :blur
-    edited.trigger(:blur)
-  end
-  if options.has_key?(:author)
-    version=options[:author]
-  end
-  read_translation version if not previously_in_edit_mode
-end
-
-def create_translation(version)
-  debug "click on add version button"
-  page.find("a#addVersion").trigger(:click)
-  debug "fill the creator #{version}"
-  fill_in :'work-creator', :with => version
-  begin
-    debug "click on create button"
-    #why do we need to click twice ????
-    page.find('input[name=do-create]').click
-    page.find('input[name=do-create]').click
-    debug "wait #{version} to appear"
-  end until has_translation?(version)
-  debug "created #{version}"
 end
