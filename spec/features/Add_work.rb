Hypertopic/TraduXio

commit b9481d9e49b5ab81ac348330c865145d19235043

FIX: Get error if trying to edit missing translation.

couchdb/updates/work.js
@@ -5,6 +5,9 @@ function(work, req) {
   } catch (e) {
     args={};
   }
+  if (["PUT","POST","DELETE"].indexOf(req.method)==-1) {
+    return [null,{code:405,body:"Method "+req.method+" not allowed"}];
+  }
   if (work===null) {
     work=args;
     work._id=work.id || req.id || req.uuid;
@@ -34,8 +37,8 @@ function(work, req) {
     orignal=true;
   } else {
     if(!work.translations[version_name]) {
-      if (req.method=="DELETE") {
-        return [work,{code:404,body:version+" not found"}];
+      if (req.method=="DELETE" || req.method=="PUT") {
+        return [null,{code:404,body:version_name+" not found"}];
       }
       var l = 1;
       if (work.text) l=work.text.length;
@@ -48,9 +51,13 @@ function(work, req) {
       }
       var text=[];
       for(var i=0 ; i<l ; i++) {
-          text.push("");
+        text.push("");
       }
       work.translations[version_name] = { title: "", language: "", creator:"", text: text };
+    } else {
+      if (req.method=="POST") {
+        return [null,{code:400,body:version_name+" already exists"}];
+      }
     }
     doc = work.translations[version_name];
   }
@@ -82,13 +89,18 @@ function(work, req) {
       version_name=new_name;
     }
   }
+  var keysOK=["date","language","title","text","creativeCommons"];
   for (var key in args) {
-    if (doc[key] && doc[key] != args[key]) {
-      actions.push("change "+key+" from "+doc[key]+" to "+args[key]+" for "+version_name);
+    if (keysOK.indexOf(key)!=-1) {
+      if (doc[key] && doc[key] != args[key]) {
+        actions.push("change "+key+" from "+doc[key]+" to "+args[key]+" for "+version_name);
+          doc[key]=args[key];
+      } else if (!doc[key]) {
+        actions.push("set "+key+" to "+args[key]+" for "+version_name);
         doc[key]=args[key];
-    } else if (!doc[key]) {
-      actions.push("set "+key+" to "+args[key]+" for "+version_name);
-      doc[key]=args[key];
+      }
+    } else {
+      return [work,{code:400,body:"can't set value for "+key}];
     }
   }
   return [work, JSON.stringify(actions)];

couchdb/_attachments/js/work.js
@@ -391,7 +391,7 @@ function addVersion() {
   var ref = $("#addPanel").find("input[name='work-creator']").val();
   if(ref != "") {
     request({
-      type: "PUT",
+      type: "POST",
       url: "work/"+id+"/"+ref,
       contentType: 'text/plain',
       data: JSON.stringify({creator: ref})

commit f539bb3529f274d3802479a1b51398074067cad6

FIX: Can edit original version metadata again.
Feature included in 7851c24 and broken in b9481d9.
Added test to avoid future break.

couchdb/templates/work.html
@@ -14,7 +14,7 @@
       <input class="work-creator" name="work-creator" placeholder="{{i_author}}" value="{{work_creator}}">
       <select class="language" name="language" placeholder="{{i_original_language}}" data-language="{{work_language}}"></select>
       <input class="date" name="date" placeholder="{{i_year_original}}" value="{{date}}">
-      <label>{{i_original}}<input type="checkbox" name="original-work" {{#original}}checked="checked" {{/original}}></label>
+      <label>{{i_original}}<input {{#id}}disabled="disabled"{{/id}} type="checkbox" name="original-work" {{#original}}checked="checked" {{/original}}></label>
       <input type="submit" value="{{i_save}}">
     </form>
   {{/notext}}

couchdb/updates/work.js
@@ -30,11 +30,11 @@ function(work, req) {
     }
   }
 
-  var doc;
-  original=false;
+  var doc,
+      original=false;
   if(!version_name || version_name == "original") {
     doc = work;
-    orignal=true;
+    original=true;
   } else {
     if(!work.translations[version_name]) {
       if (req.method=="DELETE" || req.method=="PUT") {
@@ -93,6 +93,15 @@ function(work, req) {
       version_name=new_name;
     }
     result.creator=version_name;
+  }
+  if(original && args.hasOwnProperty("original")) {
+    var has_original=work.text?true:false;
+    args.original=args.original?true:false;
+    if (args.original!=has_original) {
+      //here we can add the original version
+      return [work,{code:400,body:"can't add or remove original version"}];
+    }
+    delete args.original;
   }
   var keysOK=["date","language","title","text","creativeCommons"];
   for (var key in args) {

spec/features/Add_work.rb
@@ -6,6 +6,7 @@ feature 'Add a work' do
     create_work data
     expect(page).to have_content "#{data[:title]} – #{data[:author]}"
     expect(page).not_to have_content 'Trans.'
+    expect(page).not_to have_selector 'a#editDoc'
     insert_work_text sample('the_lamp')
     expect(row(1)).to have_content 'THE LAMP'
     expect(row(2)).to have_content 'We found the lamp'
@@ -19,6 +20,7 @@ feature 'Add a work' do
     create_work data
     expect(page).to have_content "#{data[:title]} – Anonymus"
     expect(page).to have_content 'Trans.'
+    expect(page).to have_selector 'a#editDoc'
   end
 
   scenario 'Without an original version (and a known author)' do
@@ -27,6 +29,23 @@ feature 'Add a work' do
     create_work data
     expect(page).to have_content "#{data[:title]} – #{data[:author]}"
     expect(page).to have_content 'Trans.'
+    expect(page).to have_selector 'a#editDoc'
+  end
+
+  scenario 'Edit the unexisting original version' do
+    data=random_work
+    data[:no_original]=true
+    create_work data
+    expect(page).to have_content "#{data[:title]} – #{data[:author]}"
+    expect(page).to have_content 'Trans.'
+    expect(page).to have_selector 'a#editDoc'
+    find('a#editDoc').trigger(:click)
+    newdata=random_work
+    newdata[:no_original]=true
+    edit_work newdata
+    expect(page).to have_content "#{newdata[:title]} – #{newdata[:author]}"
+    expect(page).to have_content 'Trans.'
+    expect(page).to have_selector 'a#editDoc'
   end
 
 end

spec/work_helper.rb
@@ -41,10 +41,6 @@ end
 
 def create_work(options)
   visit '/works/new'
-  fill_in 'Title', :with => options[:title] if options.has_key?(:title)
-  fill_in 'Author', :with => options[:author] if options.has_key?(:author)
-  fill_select 'language',options[:language] if options.has_key?(:language)
-  fill_in 'Date, year, or text century', :with=>options[:date] if options.has_key?(:date)
   if options.has_key?(:no_original) && options[:no_original] then
     debug "no original"
     uncheck 'Original work'
@@ -52,12 +48,21 @@ def create_work(options)
     debug "original"
     check 'Original work'
   end
-  find('input[value=Save]').trigger(:click)
-  wait_for_ajax
+  edit_work options
   debug "Created #{options[:title]} – #{options[:author]}"
   if options.has_key?(:no_original) && options[:no_original] then
     edit_translation_metadata "first",random_translation_metadata
   end
+end
+
+def edit_work(options)
+  debug options
+  fill_in 'Title', :with => options[:title] if options.has_key?(:title)
+  fill_in 'Author', :with => options[:author] if options.has_key?(:author)
+  fill_select 'language',options[:language] if options.has_key?(:language)
+  fill_in 'Date, year, or text century', :with=>options[:date] if options.has_key?(:date)
+  find('input[value=Save]').trigger(:click)
+  wait_for_ajax
 end
 
 def insert_work_text (text)